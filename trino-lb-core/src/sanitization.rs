4527c9b409f88e9db4a39062a20099708c8abdae
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2024-01-18 09:35:16 +0100

    test: Add test for header sanitization

--- trino-lb-core/src/sanitization.rs
+++ trino-lb-core/src/sanitization.rs
@@ -11,3 +11,32 @@ impl Sanitize for http::HeaderMap {
         sanitized
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use http::{
+        header::{CONTENT_LENGTH, HOST},
+        HeaderMap, HeaderValue,
+    };
+
+    use super::*;
+
+    #[test]
+    fn test_sanitize() {
+        let mut headers = HeaderMap::new();
+        headers.insert(HOST, "example.com".parse().unwrap());
+        headers.insert(CONTENT_LENGTH, "123".parse().unwrap());
+
+        let sanitized = headers.sanitize();
+        assert_eq!(sanitized, headers);
+
+        headers.insert("Authorization", HeaderValue::from_static("secure"));
+        let sanitized = headers.sanitize();
+        assert_eq!(sanitized.get("Authorization").unwrap(), "<redacted>");
+
+        // Also test lowercase variant
+        headers.insert("authorization", HeaderValue::from_static("secure"));
+        let sanitized = headers.sanitize();
+        assert_eq!(sanitized.get("authorization").unwrap(), "<redacted>");
+    }
+}
